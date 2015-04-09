6242b2b98fed92ba4056dbf023cd8a82bbcc7d99
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-04-09 13:01:11 -0400

    Added dynamic text via jquery

--- client/js/portfolio.js
+++ client/js/portfolio.js
@@ -6,7 +6,20 @@ Template.carousel.helpers({
 Template.carousel.events({
 
 'click .carouselItem' : function (e) {
-		console.log(e.target)        
+		console.log(e.currentTarget.id)
+		$(".infoItem").css("visibility","hidden");
+		switch(e.currentTarget.id) {
+    	case "item1":
+        	$("#text1").css("visibility","visible");
+        break;
+    	case "item2":
+        	$("#text2").css("visibility","visible");
+        break;
+        case "item3":
+        	$("#text3").css("visibility","visible");
+        break;
+		}
+
     }
 
 });
