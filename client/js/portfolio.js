f20d77bb974bb8a4443834841bdb2c6c06529d77
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-05-26 16:40:12 -0400

    Added cerebro stuff

--- client/js/portfolio.js
+++ client/js/portfolio.js
@@ -5,24 +5,33 @@ Template.carousel.helpers({
 
 Template.carousel.events({
 
-'click .carouselItem' : function (e) {
-		console.log(e.currentTarget.id)
-		$(".infoItem").css("display","none");
-        $("#InfoPane").css("visibility","visible");
+'click .carouselItem' : function (e) {        
+        //Set all of the infoItems to invsible before making clicked visible.
+        //Jquery Method
+        //$(".infoItem").css("display","none");
+        //$("#InfoPane").css("visibility","visible");
+        
+        //Pure Javascript Method
+        var items = document.getElementsByClassName("infoItem");
+        for(var i=0;i<items.length;i++){
+            items[i].style.display = "none";
+        }
+        
+        document.getElementById("InfoPane").style.visibility = "visible";
+    
+        
 		switch(e.currentTarget.id) {
     	case "item1":
-        	$("#app1").css("display","inline-block");
+            document.getElementById("app1").style.display = "inline-block";
         break;
     	case "item2":
-        	$("#app2").css("display","inline-block");
+            document.getElementById("app2").style.display = "inline-block";
         break;
         case "item3":
-        	$("#app3").css("display","inline-block");
+            document.getElementById("app3").style.display = "inline-block";
         break;
 		}
-
     }
-
 });
 
 
