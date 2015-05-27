613930be1e4614446ef2ea105abd5529ad4442c0
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-05-26 20:47:55 -0400

    Fixed formatting issue

--- client/js/portfolio.js
+++ client/js/portfolio.js
@@ -13,6 +13,7 @@ Template.carousel.events({
         
         //Pure Javascript Method
         var items = document.getElementsByClassName("infoItem");
+        
         for(var i=0;i<items.length;i++){
             items[i].style.display = "none";
         }
